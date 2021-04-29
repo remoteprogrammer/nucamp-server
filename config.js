e2715374cab3455f382f3bea1e5340540f9308d0
Scott Lund <scottl@cceast.com>
2021-04-29 11:22:09 -0400

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  secretKey: "12345-67890-09876-54321",
-  mongoUrl: "mongodb://localhost:27017/nucampsite",
-  facebook: {
-    clientId: "476182753645946",
-    clientSecret: "8f75bdecc29b9380e21450d6a8bb2e42",
-  },
-};
