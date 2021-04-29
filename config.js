7437cfb0c402c81e05a97bc0f896d264d0f50b1f
Scott Lund <scottl@cceast.com>
2021-04-29 10:44:36 -0400

    Added Facebook Login OAuth2

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
   secretKey: "12345-67890-09876-54321",
   mongoUrl: "mongodb://localhost:27017/nucampsite",
+  facebook: {
+    clientId: "476182753645946",
+    clientSecret: "8f75bdecc29b9380e21450d6a8bb2e42",
+  },
 };
